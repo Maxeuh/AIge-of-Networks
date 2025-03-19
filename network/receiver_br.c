7c622f70f9cf56b5e2eff40946c21b7203f5d373
unknown <yassinebelgacem02@gmail.com>
2025-03-19 16:37:14 +0100

    receiving second machine

---
+++ network/receiver_br.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define UDP_PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Création socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Erreur socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configuration pour recevoir le broadcast
+    int broadcast_enable = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, 
+                   &broadcast_enable, sizeof(broadcast_enable)) < 0) {
+        perror("Erreur activation broadcast");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configuration adresse serveur
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;  // Écoute toutes les interfaces
+    server_addr.sin_port = htons(UDP_PORT);
+
+    // Bind
+    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Erreur bind");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("En attente de messages broadcast sur le port %d...\n", UDP_PORT);
+
+    while (1) {
+        int recv_len = recvfrom(sock, buffer, BUFFER_SIZE, 0,
+                               (struct sockaddr*)&client_addr, &addr_len);
+        
+        if (recv_len > 0) {
+            buffer[recv_len] = '\0';
+            printf("Message reçu de %s: %s\n", 
+                   inet_ntoa(client_addr.sin_addr), buffer);
+        }
+    }
+
+    close(sock);
+    return 0;
+}
